0cfd3d3ede8feec5e6d4148257b6e99c0f256e12
Man Nguyen <nmman37@gmail.com>
2022-10-20 22:21:25 +0700

    Configured to build boot.img
    Signed-off-by: Man Nguyen <nmman37@gmail.com>
    Change-Id: I43a82ebfdb4af20de05f154c883a5e57a31bb415

--- orangepi4_lts/AndroidBoard.mk
+++ orangepi4_lts/AndroidBoard.mk
@@ -3,3 +3,7 @@ UBOOT_DEFCONFIG := orangepi_4_defconfig
 UBOOT_ROCKCHIP_SPL := true
 
 include $(TOP)/device/xunlong/common/uboot_rk.mk
+include $(TOP)/device/xunlong/common/kernel.mk
+
+# Include uboot and kernel to default target
+droidcore-unbundled: uboot_full kernel
