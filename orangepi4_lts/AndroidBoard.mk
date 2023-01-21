7686d241831c83eed5c57ea009bc13493a872824
Man Nguyen <nmman37@gmail.com>
2023-01-21 22:01:13 +0700

    Build device tree blob
    Change-Id: I835ebef99f0ec7c54a2d4c8a6785b6b17781d5f5
    Signed-off-by: Man Nguyen <nmman37@gmail.com>

--- orangepi4_lts/AndroidBoard.mk
+++ orangepi4_lts/AndroidBoard.mk
@@ -3,6 +3,10 @@ UBOOT_DEFCONFIG := orangepi_4_defconfig
 UBOOT_ROCKCHIP_SPL := true
 
 include $(TOP)/device/xunlong/common/uboot_rk.mk
+
+KERNEL_CONFIG_FRAGMENTS := $(TOP)/device/xunlong/orangepi4/orangepi4_lts/kernel.config
+KERNEL_DTB := rockchip/rk3399-orangepi.dtb
+
 include $(TOP)/device/xunlong/common/kernel.mk
 
 # Include uboot and kernel to default target
