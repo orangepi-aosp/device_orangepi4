c67c433e7e0ff9ecd064cee0229c590cc59775d8
Man Nguyen <nmman37@gmail.com>
2023-01-28 12:40:21 +0700

    Add fastboot.sh script
    Change-Id: I03fec0498c08d7fa7e1308b94949eb356a414d7e
    Signed-off-by: Man Nguyen <nmman37@gmail.com>

--- device.mk
+++ device.mk
@@ -1,2 +1,4 @@
 DEVICE_MANIFEST_FILE := device/xunlong/orangepi4/manifest.xml
 DEVICE_MATRIX_FILE += device/xunlong/orangepi4/compatibility_matrix.xml
+
+PRODUCT_COPY_FILES += device/xunlong/orangepi4/fastboot.sh:$(PRODUCT_OUT)/fastboot.sh
---
+++ fastboot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+fastboot flash loader1 ./obj/UBOOT_OBJ/rktmp/idbloader.img
+fastboot flash loader2 ./obj/UBOOT_OBJ/rktmp/uboot.img
+fastboot flash trust ./obj/UBOOT_OBJ/rktmp/trust.img
+fastboot flash boot ./boot-5.15.img
+fastboot flash init_boot ./init_boot.img
