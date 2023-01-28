00d49a9afc1a92be14f5e0f1b2f544895941bfd1
Man Nguyen <nmman37@gmail.com>
2023-01-28 12:41:34 +0700

    Configure init_boot image
    Change-Id: I00c0ada33b9a458e8d526ef467cecbe75fc319f1
    Signed-off-by: Man Nguyen <nmman37@gmail.com>

--- orangepi4_lts/AndroidBoard.mk
+++ orangepi4_lts/AndroidBoard.mk
@@ -4,7 +4,10 @@ UBOOT_ROCKCHIP_SPL := true
 
 include $(TOP)/device/xunlong/common/uboot_rk.mk
 
-KERNEL_CONFIG_FRAGMENTS := $(TOP)/device/xunlong/orangepi4/orangepi4_lts/kernel.config
+KERNEL_VERSION := 5.15
+KERNEL_SRC := $(TOP)/kernel/android13-5.15-lts
+KERNEL_CONFIG_FRAGMENTS := $(TOP)/device/xunlong/orangepi4/orangepi4_lts/opi4lts.fragment
+KERNEL_CONFIG_FRAGMENTS += $(KERNEL_SRC)/arch/arm64/configs/rockpi4_gki.fragment
 KERNEL_DTB := rockchip/rk3399-orangepi.dtb
 
 include $(TOP)/device/xunlong/common/kernel.mk
